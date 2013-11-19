38a8da897e434ac8db0dd7cbe15ef369d25dd4bf
Kevin <etienne.kevin@gmail.com>
2013-11-19 20:37:07 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-bootstrap-sass',
-    version='0.0.1-alpha',
+    version='0.0.2-alpha',
     author='Kevin Etienne',
     author_email='etienne.kevin@gmail.com',
     url='http://github.com/ekevin/django-bootstrap-sass',
     license='Apache licence, see LICENCE',
     description='Bootstrap sass file for django',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     packages=['bootstrap_sass'],
-    package_data={'bootstrap': ['static/javascripts/*',
-                                'static/stylesheets/*',
-                                'static/images/*']},
+    package_data={'bootstrap_sass': ['static/javascripts/*',
+                                     'static/stylesheets/*',
+                                     'static/images/*']},
 )
 
 
