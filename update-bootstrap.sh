f667582c44d4def2b20447f270230c2660484709
Kevin <etienne.kevin@gmail.com>
2013-11-19 20:46:25 +0000

    Updated script to import bootstrap files

--- update-bootstrap.sh
+++ update-bootstrap.sh
@@ -8,17 +8,16 @@ TMP='tmp/sass-twitter-bootstrap'
 # Pull down sass-twitter-bootstrap sources
 git clone https://github.com/jlong/sass-twitter-bootstrap.git tmp/sass-twitter-bootstrap
 
+rm -r $ROOT/javascripts/*
+rm -r $ROOT/images
+rm -r $ROOT/stylesheets/*
+
 cp -r $TMP/lib/* $ROOT/stylesheets
 cp -r $TMP/js/* $ROOT/javascripts
-cp -r $TMP/img/* $ROOT/images
+cp -r $TMP/fonts/* $ROOT/fonts
 
 # Remove tests
 rm -r $ROOT/javascripts/tests
-rm -r $ROOT/stylesheets/bootstrap/tests
-
-# Update paths
-sed -i .bak 's!../img!../images!g' $ROOT/stylesheets/_variables.scss
-rm $ROOT/stylesheets/*.bak
 
 rm -rf tmp
 
